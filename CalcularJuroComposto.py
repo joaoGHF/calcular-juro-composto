5e658d546d93bcc8b5c2b8447a5e10e6d072058c
joaoGHF <99850827+joaoGHF@users.noreply.github.com>
2022-04-23 23:26:05 -0300

    Update CalcularJuroComposto.py

--- CalcularJuroComposto.py
+++ CalcularJuroComposto.py
@@ -14,12 +14,14 @@ Taxa = float(Taxa)
 Taxa = Taxa/100
 Tempo = float(Tempo)
 
-print('Usando uma fórmula onde C = Capital, i = taxa, t = tempo e M = Montante')
+print('\nUsando uma fórmula onde C = Capital, i = taxa, t = tempo e M = Montante')
 print('M = C*(1+i)^t')
 
 Montante = (Capital*(1-Taxa)**Tempo)
 
-print(f'M = {Capital}*(1 + {Taxa})^{Tempo}')
+print(f'\nM = {Capital}*(1 + {Taxa})^{Tempo}')
 print(f'M = {Capital}*({1+Taxa})^{Tempo}')
 print(f'M = {Capital}*{(1+Taxa)**Tempo}')
-print(f'Montante = {Capital*(1+Taxa)**Tempo}')
+print(f'\nMontante = {Capital*(1+Taxa)**Tempo}')
+
+_ = input("Pressione 'Enter' para encerrar o programa...")
